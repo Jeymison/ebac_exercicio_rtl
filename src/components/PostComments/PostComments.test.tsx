8cbabf39189ce34f65a72aa1ccd7d27e0d5da3c9
Jeymison Menezes <jeymisong@gmail.com>
2024-04-13 18:46:14 -0300

    Incluindo teste para comentarios

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,29 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
 import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
     it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
+        render(<PostComment />);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
-});
+
+    it('Adiciona dois comentarios novos', () => {
+        render(<PostComment />);
+        
+        fireEvent.change(screen.getByTestId('textarea'), {
+            target: {
+                value: 'Alo curso da Ebac!'
+            }
+        });
+        fireEvent.click(screen.getByTestId('button'));
+
+        fireEvent.change(screen.getByTestId('textarea'), {
+            target: {
+                value: 'Bem vindo ao Curso de Python!'
+            }
+        });
+        fireEvent.click(screen.getByTestId('button'));
+        expect(screen.getAllByTestId('textos')).toHaveLength(2); 
+    });  
+}); 
+
